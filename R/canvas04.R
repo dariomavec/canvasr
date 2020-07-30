5bcf3a3f15d2eccc479f36b7c988ded4a75762b2
Dario Mavec <dario.mavec@catena4.com>
2020-07-30 13:57:38 +1000

    Add canvas04

---
+++ R/canvas04.R
@@ -0,0 +1,56 @@
+#' Canvas 4
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' @import dplyr
+#' @import tidyr
+#' @import purrr
+#' @importFrom glue glue
+canvas04 <- function(
+  width = 300,
+  height = 500,
+  bg_color = '#e62739',
+  file_name = 'canvas04',
+  ...
+  )
+{
+  plot_centre <- c(0, 0)
+  size <- 20
+
+  grid <- expand_grid(
+    x = seq(-width/2, width/2, by = size * 2),
+    y = seq(-height/2, height/2, by = size * 2),
+    r = size / 2,
+    r0 = 0
+  )
+  grid <- bind_rows(grid,
+                    mutate(grid,
+                           x = x + size,
+                           y = y + size))
+
+  plot <- ggplot() +
+    geom_arc_bar(aes(x0 = x, y0 = y, r0 = r0, r = r, start = 3 * pi / 4, end = 7 * pi / 4),
+                 data = grid,
+                 size = 0,
+                 fill = '#9068be') +
+    geom_arc_bar(aes(x0 = x, y0 = y, r0 = r0, r = r, start = -1 * pi / 4, end = 3 * pi / 4),
+                 data = mutate(grid, x = x + size/2, y = y - size/2),
+                 size = 0,
+                 fill = '#6ed3cf') +
+    theme_void() +
+    theme(
+      panel.background = element_rect(fill = bg_color),
+      plot.background = element_rect(fill = bg_color),
+      panel.grid = element_blank(),
+      panel.border = element_blank()
+      ) +
+    coord_equal(xlim = c(-width/2, width/2),
+                ylim = c(-height/2, height/2),
+                expand = FALSE)
+
+  save_plot(plot, file_name, height = height, width = width, ...)
+}
+
+canvas04()
