27f793cb2a2fd477655faa3f473f74ca87a5b11f
Dario Mavec <dario.mavec@catena4.com>
2020-08-04 10:55:02 +1000

    More expressive naming standard

---
+++ R/11-breton.R
@@ -0,0 +1,42 @@
+#' Canvas 10
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' @import dplyr
+#' @import tidyr
+#' @import purrr
+#' @importFrom glue glue
+canvas10 <- function(
+  width = 300,
+  height = 500,
+  bg_color = '#ffffff',
+  accent_color = '#933A16',
+  file_name = 'canvas10',
+  ...
+  )
+{
+  plot_centre <- c(0, 0)
+
+  grid <- expand_grid(
+    x = seq(-width/2, width/2, by = width / 10),
+    y = seq(-height/2, height/2, by = height / 10)
+  )
+
+  plot <- grid %>%
+    ggplot() +
+    theme_void() +
+    theme(
+      panel.background = element_rect(fill = bg_color),
+      plot.background = element_rect(fill = bg_color),
+      panel.grid = element_blank(),
+      panel.border = element_blank(),
+      legend.position = 'none'
+      ) +
+    coord_equal(xlim = c(-width/2, width/2),
+                ylim = c(-height/2, height/2),
+                expand = FALSE)
+
+  save_plot(plot, file_name, height = height, width = width, ...)
+}
