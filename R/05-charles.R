80ad661f427097b6317700aa3eb73d391c148b7b
Dario Mavec <dario.mavec@catena4.com>
2020-09-24 13:17:28 +1000

    Tweaks to canvas 05

--- R/05-charles.R
+++ R/05-charles.R
@@ -1,9 +1,6 @@
 #' Canvas 5
 #'
-#' @return
 #' @export
-#'
-#' @examples
 #' @import dplyr
 #' @import tidyr
 #' @import purrr
@@ -12,74 +9,63 @@ canvas05 <- function(
   width = 300,
   height = 500,
   bgColour = '#e62739',
-  file_name = '05-charles',
+  fileName = '05-charles',
+  gridSpacing = 20,
+  circleRadius = gridSpacing / 2,
   ...
   )
 {
   plot_centre <- c(0, 0)
-  size <- 20
 
   grid <- expand_grid(
-    x = seq(-width/2, width/2, by = size * 2),
-    y = seq(-height/2, height/2, by = size * 2),
-    r = size / 2,
+    x = seq(-width/2, width/2, by = gridSpacing * 2),
+    y = seq(-height/2, height/2, by = gridSpacing * 2),
+    r = circleRadius,
     r0 = 0
   )
-  grid <- bind_rows(grid,
-                    mutate(grid,
-                           x = x + size,
-                           y = y + size))
 
-  n_cutouts <- 3
-  # cutouts_2 <- tibble(
-  #   x = c(100, -100, 60),
-  #   y = c(100, 140, -60),
-  #   r = c(30, 35, 30)
-  #   # x = width * runif(n_cutouts, -1/2, 1/2),
-  #   # y = height * runif(n_cutouts, -1/2, 1/2),
-  #   # r = height/10 * runif(n_cutouts, 1/2, 1)
-  # )
-  cutouts_1 <- grid %>%
-    slice_sample(n = 15) %>%
-    mutate(x = x + size / 2,
-           y = y - size / 2) %>%
-    bind_rows(slice_sample(grid, n = 15))
-  # cutouts_2 <- tibble(
-  #   x = 0,
-  #   y = 120,
-  #   r = 30
-  # )
+  grid <- grid %>%
+    bind_rows(
+      mutate(grid,
+             x = x + gridSpacing,
+             y = y + gridSpacing)
+    )
 
-  plot <- ggplot() +
+  diamond_cutout <- square(0, 0, 113) %>%
+    rotate(pi/4) %>%
+    .$pts
+
+  # n_cutouts <- 3
+  # cutouts_1 <- grid %>%
+  #   slice_sample(n = 15) %>%
+  #   mutate(x = x + gridSpacing / 2,
+  #          y = y - gridSpacing / 2) %>%
+  #   bind_rows(slice_sample(grid, n = 15))
+
+  plot <- ggcanvas(width = width, height = height, bgColour = bgColour) +
     geom_arc_bar(aes(x0 = x, y0 = y, r0 = r0, r = r, start = 3 * pi / 4, end = 7 * pi / 4),
                  data = grid,
                  size = 0,
                  fill = '#9068be') +
     geom_arc_bar(aes(x0 = x, y0 = y, r0 = r0, r = r, start = -1 * pi / 4, end = 3 * pi / 4),
-                 data = mutate(grid, x = x + size/2, y = y - size/2),
+                 data = mutate(grid, x = x + gridSpacing/2, y = y - gridSpacing/2),
                  size = 0,
                  fill = '#6ed3cf') +
-    geom_circle(aes(x0=x, y0=y, r=r),
-                data = cutouts_1,
-                fill = bgColour,
-                size = 0) +
+    geom_polygon(aes(x, y),
+                 data = diamond_cutout,
+                 fill = bgColour,
+                 size = 0)
+    # geom_circle(aes(x0=x, y0=y, r=r),
+    #             data = cutouts_1,
+    #             fill = bgColour,
+    #             size = 0) +
     # geom_circle(aes(x0=x, y0=y, r=r),
     #             data = cutouts_2,
     #             fill = '#6ed3cf',
     #             colour = '#9068be',
     #             size = 0.5)  +
-    theme_void() +
-    theme(
-      panel.background = element_rect(fill = bgColour),
-      plot.background = element_rect(fill = bgColour),
-      panel.grid = element_blank(),
-      panel.border = element_blank()
-      ) +
-    coord_equal(xlim = c(-width/2, width/2),
-                ylim = c(-height/2, height/2),
-                expand = FALSE)
 
-  save_plot(plot, file_name, height = height, width = width, ...)
+  save_plot(plot, fileName, height = height, width = width, ...)
 }
 
-# canvas05()
+canvas05()
